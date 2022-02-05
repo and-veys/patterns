5c603280b10d02ba5b45db6290851c38cd95ac84
veys <and.veys@gmail.com>
2022-02-05 21:44:10 +0300

    Menu

--- homework.cpp
+++ homework.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "menu.h"
 
 int main() {
-
+	Client cl;
+	std::vector<Menu *> vec;
+	vec.push_back(new MenuItem("Borsch", "Beetroot soup", 3.25));
+	vec.push_back(new Ingredient("Beetroot", 50, 50.0, 5.2, 1.8, 5.1));
+	vec.push_back(new Ingredient("Beef", 10, 250.0, 25.4, 13.2, 7.2));
+	
+	for(Menu * m : vec) {
+		m->info(&cl);
+		std::cout << "--------------------------" << std::endl;
+	}
+	
+	for(Menu * m : vec)
+		delete m;	
 	return 0;
 }
---
+++ menu.h
@@ -0,0 +1,71 @@
+#include <vector>
+
+class Ingredient;
+class MenuItem;
+
+class Visitor {
+public:
+    virtual void visit(Ingredient*) = 0;
+    virtual void visit(MenuItem*) = 0;
+};
+
+class Menu{	
+public:
+	virtual void info(Visitor *) = 0;
+};
+
+class Ingredient: public Menu{
+private:
+   std::string name;
+   int healthRating;
+   float calories;
+   float protein;
+   float carbs;
+   float fats;
+public:
+	Ingredient(const std::string &nm, const int hr, const float cal, const float prt, const float crb, const float ft) :
+		name(nm), healthRating(hr), calories(cal), protein(prt), carbs(crb), fats(ft) {}
+	const std::string &getName() const { return name; }
+	const int getHealthRating() const { return healthRating; }
+	const float getCalories() const { return calories; }
+	const float getProtein() const { return protein; }
+	const float getCarbs() const { return carbs; }
+	const float getFats() const { return fats; }
+	
+	void info(Visitor * v) override { v->visit(this); }
+};
+
+class MenuItem: public Menu{
+private:
+   std::string name;
+   std::string description;
+   float price;
+   std::vector<Ingredient*> ingredients;
+public:
+	MenuItem(const std::string &name, const std::string &description, float price) : 
+		name(name), description(description), price(price) {}
+	const std::string &getName() const { return name; }
+	const std::string &getDescription() const { return description; }
+	float getPrice() const { return price; }
+   
+	void info(Visitor * v) override { v->visit(this); }
+};
+
+class Client : public Visitor{
+	void visit(Ingredient * ing) override {
+		std::cout 	<< "Name: " << ing->getName() 
+					<< "\nHealthRating: " << ing->getHealthRating()
+					<< "\nCalories:" << ing->getCalories() 
+					<< "\nProtein: " << ing->getProtein()
+					<< "\nCarbs: " << ing->getCarbs()
+					<< "\nFats: " << ing->getFats()
+					<< std::endl;
+	}
+	void visit(MenuItem* mi) override {
+		std::cout 	<< "Name: " << mi->getName() 
+					<< "\nDescription: " << mi->getDescription()
+					<< "\nPrice:" << mi->getPrice() 
+					<< std::endl;
+	}
+};
+
