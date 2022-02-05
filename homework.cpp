ab9f2da1e5564588749880197e5f0ac993030e5b
veys <and.veys@gmail.com>
2022-02-06 00:18:47 +0300

    News

--- homework.cpp
+++ homework.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-
+#include "news.h"
 int main() {
+	Herald herald;
+	Doctor dc(&herald);
+	Blacksmith bl(&herald);
+	Child ch(&herald);
+	
+	herald.notify(10, "Hello");
+	herald.getAnswer();
+	std::cout << "-------------------" << std::endl;
+	herald.notify(80, "One-Two-Three");
+	herald.getAnswer();
+	std::cout << "-------------------" << std::endl;
+	herald.remove(&dc);
+	herald.notify(50, "Bye");
+	herald.getAnswer();
 
 	return 0;
 }
